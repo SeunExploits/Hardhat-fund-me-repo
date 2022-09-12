d6b2bddf823e399e424637e2b0aa3e3e585c0af7
SeunExploits <seun7674@gmail.com>
2022-09-12 11:36:23 +0100

    Hardhat Tutorial

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+require("solidity-coverage")
+require("hardhat-deploy")
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.8",
+};
